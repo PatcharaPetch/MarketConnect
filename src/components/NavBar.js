39251b0608f85e478ee8b28b9ae0085d086d4a2f
PatcharaPetch <109201582+PatcharaPetch@users.noreply.github.com>
2023-09-18 17:05:27 +0700

    eiei

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,13 @@ class NavBar extends Component {
         return (
             <header>
                 <h1>MarketConnect</h1>
-                <div className="nav-bar">
+                <nav className="nav-bar">
                     <NavLink className={nav_class} to="/">Home <NavIcon img="/homeicon.png" /></NavLink>
                     <NavLink className={nav_class} to="/food">Food <NavIcon img="/foodicon.png" /></NavLink>
                     <NavLink className={nav_class} to="/shop">Shop <NavIcon img="/shopicon.png" /></NavLink>
                     <NavLink className={nav_class} to="/support">Support <NavIcon img="/supporticon.png" /></NavLink>
                     <NavLink className={nav_class} to="/register">Profile <NavIcon img="/profileicon.png" /></NavLink>
-                </div>
+                </nav>
             </header>
         );
     }
