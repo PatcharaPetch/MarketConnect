bc1a9071899c2c3815f0bb8b9128288fa79f5aba
siwagarnj <siwagarnj@gmail.com>
2023-09-28 16:16:29 +0700

    no1

--- server/server.js
+++ server/server.js
@@ -1,70 +1,115 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = 'https://ooitkismzzkxarjmwdeb.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://ooitkismzzkxarjmwdeb.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(cors());
 const port = 3200;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mark19982.",
-  database: "market",
-  insecureAuth: true,
-});
-
 app.get("/", (req, res) => {
-  console.log("requested");
-  connection.query("SELECT * FROM Food", (err, results, fields) => {
-    res.json(results);
-  });
+  supabase
+    .from("countries")
+    .select()
+    .then(({ data, error }) => {
+      if (error) {
+        res.status(500).json({ error });
+      } else {
+        res.status(200).json(data);
+      }
+    });
 });
 
-app.get("/register", (req, res) => {
-  console.log("requested");
-  connection.query("SELECT * FROM data_user", (err, results, fields) => {
-    res.status(200).json(results);
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
   });
+  if (error) {
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json(data);
+  }
 });
 
-app.get("/login", (req, res, next) => {
-  let options = {
-    root: __dirname,
-    headers: {
-      "Content-Type": "text/html; charset=UTF-8",
-    },
-  };
-  res.sendFile("/login.html", options, function(err) {
-    if (err) {
-      next(err);
-    } else {
-      console.log("Sent: login.html");
-    }
+app.post("/support", async (req, res) => {
+  const { name, email, contract, message } = req.body;
+  const { data, error } = await supabase.from("support").insert({
+    Name: name,
+    Email: email,
+    Contract_Number: contract,
+    Message: message,
   });
+  if (error) {
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json(data);
+  }
 });
 
-app.get("/profile", (req, res) => {
-  console.log("requested");
-  connection.query("SELECT * FROM data_user", (err, results, fields) => {
-    res.status(200).json(results);
+// app.get("/home", async(req, res) => {
+//   const {email , password} = req.body;
+//   const {data, error} = await supabase.auth.signUp(
+//     {
+//       email : email,
+//       password :password
+//     }
+//   );
+//   if (error) {
+//     res.status(500).json({ error });
+//   }else {
+//     res.status(200).json(data);
+//   }
+// });
+
+app.post("/fooddetail/:id", async (req, res) => {
+  const { email, password } = req.body;
+  const { data, error } = await supabase.select("food").select();
+  if (error) {
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json(data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
   });
+  if (error) {
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json(data);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  const { data, error } = await supabase.auth.getUser();
+  if (error) {
+    res.status(500).json({ error });
+  } else {
+    res.status(200).json(data);
+  }
 });
 
 app.get("/supabase", (req, res) => {
-  supabase.from("countries").select().then(({ data, error }) => {
-    if (error) {
-      res.status(500).json({ error });
-    } else {
-      res.status(200).json(data);
-    }
-  });
+  supabase
+    .from("countries")
+    .select()
+    .then(({ data, error }) => {
+      if (error) {
+        res.status(500).json({ error });
+      } else {
+        res.status(200).json(data);
+      }
+    });
 });
 
 app.listen(port, () => console.log(`Express app running on port ${port}`));
